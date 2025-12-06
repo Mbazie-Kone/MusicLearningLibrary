7fd3ed086356437157d6cc8fd073df02f41a14b2
M'bazie Kone <mbazie89@gmail.com>
2025-12-06 01:34:35 +0100

    Updates

--- MusicLibrary.Infrastructure/DbContexts/MusicLibraryDbContext.cs
+++ MusicLibrary.Infrastructure/DbContexts/MusicLibraryDbContext.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using MusicLibrary.Domain.Entities;
 
 namespace MusicLibrary.Infrastructure.DbContexts
 {
-    public class MusicLibraryDbContext
+    public class MusicLibraryDbContext : DbContext
     {
+        public MusicLibraryDbContext(DbContextOptions<MusicLibraryDbContext> options)
+            : base(options)
+        {
+        }
+
+        // “This property represents the MediaItems table.”
+        public DbSet<MediaItem> MediaItems { get; set; }
+
+        // Override for additional configurations
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Optional Fluent API configurations
+            modelBuilder.Entity<MediaItem>(entity =>
+            {
+                entity.HasKey(e => e.Id); // Primary key
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(200);
+                entity.Property(e => e.FileName)
+                    .IsRequired()
+                    .HasMaxLength(255);
+                entity.Property(e => e.FileType)
+                    .HasMaxLength(50);
+                entity.Property(e => e.FileSize).HasMaxLength(100);
+                entity.Property(e => e.UploadedAt)
+                    .HasDefaultValueSql("GETDATE()"); // Default value
+            });
+        }
     }
 }
