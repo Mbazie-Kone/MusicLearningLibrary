658dbd206284b40ae57bac53132942468cfc2e6a
M'bazie Kone <mbazie89@gmail.com>
2026-01-26 21:50:12 +0100

    feat(infrastructure): add MediaAnalysis repository implementation

---
+++ MusicLearningLibrary.Infrastructure/Repositories/MediaAnalysisRepository.cs
@@ -0,0 +1,41 @@
+﻿using MusicLearningLibrary.Application.Abstractions;
+using MusicLearningLibrary.Domain.Entities;
+using MusicLearningLibrary.Infrastructure.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLearningLibrary.Infrastructure.Repositories
+{
+    public class MediaAnalysisRepository : IMediaAnalysisRepository
+    {
+        private readonly MusicLearningLibraryDbContext _db;
+
+        public MediaAnalysisRepository(MusicLearningLibraryDbContext db)
+        {
+            _db = db;
+        }
+
+        public void add(MediaAnalysis analysis)
+        {
+            _db.MediaAnalyses.Add(analysis);
+            _db.SaveChanges();
+        }
+
+        public MediaAnalysis GetById(Guid id)
+        {
+            return _db.MediaAnalyses.Single(x => x.Id == id);
+        }
+
+        public void Update(MediaAnalysis analysis)
+        {
+            _db.MediaAnalyses.Update(analysis);
+            _db.SaveChanges();
+        }
+
+        // Helper useful for workers (if needed in tests/workers)
+
+    }
+}
